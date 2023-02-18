7cde6d340781967591498676c821d09563a61765
Marcos Chinchilla <marcoschinchilla96@gmail.com>
2023-02-17 19:41:42 -0600

    terminando la maquetacion con un poco de JavaScript

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 const references = document.querySelectorAll("li"); 
 const divOver = document.querySelectorAll("div#bar")
-console.log(divOver)
 
 references.forEach(reference => {
     const div = document.createElement("div");
-    div.classList.add("none")
-    reference.insertAdjacentElement("afterbegin", div)});
+    div.classList.add("inactive")
+    div.textContent = 52 ;
+    reference.insertAdjacentElement("afterbegin", div)
+});
 
 divOver.forEach(div =>{
     div.addEventListener("mouseover", hover)
-})
-div.Over.forEach(div =>{
-    div.addEventListener("mouseout", unHover)
-})
-function unHover(){
-    e.target.classList.add("inactive")
-}
+});
 
 function hover(e){
-    e.target.classList.toggle("isItSight");
+    const div = e.target.previousElementSibling;
+    div.classList.add("isItSight");
+    div.classList.remove("inactive")
 }
+
+divOver.forEach(div =>{
+    div.addEventListener("mouseout", unHover)
+});
+
+function unHover(e){
+    const div = e.target.previousElementSibling;
+    div.classList.remove("isItSight");
+    div.classList.add("inactive")
+}
+
