670acd1f7707f5eab9ab4ccdaa9deef697f6e01b
Marcos Chinchilla <marcoschinchilla96@gmail.com>
2023-02-19 13:49:16 -0600

    terminando proyecto

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 const references = document.querySelector("ul"); 
 import {data} from '../data.js';
 
-data.forEach( day => {
-        const item = 
+
+let amounts = [];
+data.forEach( item => {
+    amounts.push(item.amount)
+});
+data.forEach( item => {
+        const listElement = 
             `
-            <div class="inactive">${day.amount}</div>
+            <div class="inactive">${item.amount}</div>
             <div id="bar"></div>
-            <span>${day.day}</span>            
+            <span>${item.day}</span>            
             `
         
         const li = document.createElement('li');
-        li.innerHTML = item;
+        li.innerHTML = listElement;
         references.appendChild(li);
 
-        const bar = li.querySelector('#bar'); 
+        const bar = li.querySelector('#bar');
         bar.addEventListener('mouseover', hover)
         bar.addEventListener('mouseout', unHover)
+
+        const bigger = Math.max(...amounts)
+        const height = (140 * item.amount) / bigger;
+        bar.style.height = `${height}px`
 });
+
+
+
 
 
 function hover(e){
